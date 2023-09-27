e04b8af8a1c1c9cd3e8f46b9d61eb830990585d0
roderickrivero <roderick_rivero@yahoo.com>
2023-09-27 11:44:02 +0800

    restore

--- tests/jumpnrun/common/js/webgl-2d.js
+++ tests/jumpnrun/common/js/webgl-2d.js
@@ -293,10 +293,6 @@
 
           var gl = gl2d.gl = gl2d.canvas.$getContext("webgl2") || gl2d.canvas.$getContext("webgl");
 
-          console.log("Webgl initialized in lost state.");
-          var ext = gl.getExtension("WEBGL_lose_context");
-          ext.restoreContext();
-
           const iPhone = /iPhone/i.test(navigator.userAgent);
           const iPad = /iPad/i.test(navigator.userAgent);
           const iOS = iPhone || iPad;
@@ -305,13 +301,11 @@
             loseContextApi = gl.getExtension("WEBGL_lose_context");
           }
 
-          console.log("Webgl 1");
           const handleContextLost = function(event) {
             event.preventDefault();
             lostContext = true;
             console.warn("Webgl context lost.");
           }
-          console.log("Webgl 2");
         
           const handleContextRestored = function(event) {
             if (!canvas.gl2d) { return; }
@@ -341,12 +335,8 @@
             console.warn("Webgl context restored.");
           }
 
-          console.log("Webgl 3");
-
           canvas.addEventListener("webglcontextlost", handleContextLost, false);
           canvas.addEventListener("webglcontextrestored", handleContextRestored, false);
-
-          console.log("Webgl 4");
 
           gl2d.initShaders();
           gl2d.initBuffers();
@@ -364,8 +354,6 @@
           gl2d.maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
 
           lostContext = false;
-
-          console.log("Webgl 5");
 
           return gl;
         }
