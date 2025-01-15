766d2a7baf54e1e1f4cbaf36932ce643c513572a
roderickrivero <roderick_rivero@yahoo.com>
2025-01-15 18:46:34 +0800

    reflow

--- tests/fullscreen/types/fullscreen-iphone.d.ts
+++ tests/fullscreen/types/fullscreen-iphone.d.ts
@@ -1,10 +1,17 @@
 import { Fullscreen } from './fullscreen';
 export declare class FullscreenIphone implements Fullscreen {
     private touchStartY;
+    private dvhValue;
+    private vhValue;
+    private diff;
     private div;
     constructor();
+    private getHeightValue;
+    private doMeasurements;
     private addElement;
     private addEventListeners;
+    private doFullscreen;
+    private undoFullscreen;
     private handleOrientationChange;
     private handleTouchStart;
     private handleTouchEnd;
