af2ee71a64888cd008f0654d5588cb9a15746edf
roderickrivero <roderick_rivero@yahoo.com>
2025-01-17 15:42:27 +0800

    test

--- tests/fullscreen/types/fullscreen-iphone-chrome.d.ts
+++ tests/fullscreen/types/fullscreen-iphone-chrome.d.ts
@@ -1,14 +1,11 @@
 import { Fullscreen } from './fullscreen';
 export declare class FullscreenIphoneChrome implements Fullscreen {
-    private vhValue;
     private threshold;
     private swipeDist;
     private touchStartY;
     private div;
     private id?;
     constructor();
-    private doMeasurements;
-    private getHeightValue;
     private addElement;
     private addEventListeners;
     private doFullscreen;
