6c1c7c87d58e9e8bfa0dd7a7989c99ae01dc02cb
roderickrivero <roderick_rivero@yahoo.com>
2025-01-22 14:36:11 +0800

    test

--- tests/fullscreen/types/fullscreen-iphone-chrome.d.ts
+++ tests/fullscreen/types/fullscreen-iphone-chrome.d.ts
@@ -1,6 +1,7 @@
 import { Fullscreen } from './fullscreen';
 export declare class FullscreenIphoneChrome implements Fullscreen {
     private div;
+    private id?;
     constructor();
     private addElement;
     private addEventListeners;
